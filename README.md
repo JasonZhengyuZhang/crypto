6383ccb6ffa177b00cfd691c03023b3be9ef5e45
JasonZhengyuZhang <zhengyuhood@gmail.com>
2019-07-31 16:22:46 -0400

    TEST GCP

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # crypto
 
 ##COMMAND TO IMPORT GCP CRED.
-export GOOGLE_APPLICATION_CREDENTIALS="/home/ec2-user/dev/My Project 76248-7a98ddc2eab4.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/ec2-user/dev/crypto/My Project 76248-7a98ddc2eab4.json"
--- main.py
+++ main.py
@@ -3,9 +3,7 @@ client = bigquery.Client()
 
 
 query = (
-    "SELECT name FROM `bigquery-public-data.usa_names.usa_1910_2013` "
-    'WHERE state = "TX" '
-    "LIMIT 100"
+    "SELECT timestamp FROM `bigquery-public-data.crypto_bitcoin.blocks` ORDER BY timestamp ASC"
 )
 query_job = client.query(
     query,
